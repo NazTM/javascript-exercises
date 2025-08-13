cc4d69d5dd64b9eb67a71694cc820a75a012619d
NazTM <103762441+NazTM@users.noreply.github.com>
2025-08-13 18:57:12 +0600

    edit js files

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(s, x) {
+  if (x <= 0) return "error";
+  let string = "";
+  for (let i = 0; i <= x; i++) {
+    string += s;
+  }
+  return string;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+  let arr = s.split("");
+  let string = "";
+  for (let i = arr.length - 1; i >= 0; i--){
+    string += arr[i];
+  }
+  return string;
 };
 
 // Do not edit below this line
